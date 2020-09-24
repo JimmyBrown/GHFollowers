ac568c0ee7e58993bc4fa874cc46309bcf2a87a4
Jimmy <jabro4@gmail.com>
2020-09-23 20:56:01 -0700

    Created custom containerView

---
+++ GHFollowers/Custom Views/GFContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFContainerView.swift
+//  GHFollowers
+//
+//  Created by Jimmy on 9/23/20.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
